a096e317280ba89e7e1ff99710a1db278b3a04a5
Mattias <klutten99@gmail.com>
2022-03-23 16:12:24 +0100

    skeleton

---
+++ HW/A/1.py
@@ -0,0 +1,8 @@
+import torch
+import torch_geometric.data as Data
+edge_index = torch.tensor([[0, 1, 1, 2],
+                           [1, 0, 2, 1]], dtype=torch.long)
+x = torch.tensor([[-1], [0], [1]], dtype=torch.float)
+
+data = Data(x=x, edge_index=edge_index) 
+Data(edge_index=[2, 4], x=[3, 1])
---
+++ HW/C/README.txt
@@ -0,0 +1,2 @@
+To run the simulation script (HW_C.ipynb), you will need to install deeptrack, a software for simulating microscopy images. 
+You do this through "pip install deeptrack" on the command line
