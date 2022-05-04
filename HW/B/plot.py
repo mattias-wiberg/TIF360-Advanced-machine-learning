09328513782448879b5c8d0c3378448d4ee599f4
Mattias Wiberg <klutten99@gmail.com>
2022-05-04 11:11:51 +0200

    added plots for C. fixed moving average for B plotting

--- HW/B/plot.py
+++ HW/B/plot.py
@@ -2,7 +2,17 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
-task = (1,"c")
-rewards = pickle.load(open("tasks/"+str(task[0])+"/"+task[1]+"/rewards.p", "rb"))
+def moving_average(x, w):
+    avg = []
+    for i in range(len(x)):
+        avg.append(np.mean(x[max(0, int(i-w/2)):min(len(x)-1,int(i+w/2))]))
+    return avg
+
+task = (1,"a")
+rewards = pickle.load(open("HW/B/tasks/"+str(task[0])+"/"+task[1]+"/rewards.p", "rb"))
 plt.plot(rewards)
+average = moving_average(rewards, 100)
+np.mean(average)
+plt.plot(average)
+plt.legend(["Rewards", "Moving average"])
 plt.waitforbuttonpress()
